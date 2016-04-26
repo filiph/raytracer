7fb5d26be67383efa0f179a82f2c5f7081059d2b
Filip Hracek <filip.hracek@gmail.com>
2016-04-25 17:00:33 -0700

    Go even closer to TypeScript's implementation

--- web/raytracer.dart
+++ web/raytracer.dart
@@ -2,43 +2,59 @@ import 'dart:async';
 import 'dart:html';
 import 'dart:math';
 
-import 'package:vector_math/vector_math.dart' show Vector3;
+class Vector {
+  final num x, y, z;
+  Vector(this.x, this.y, this.z);
+
+  operator *(num k) => new Vector(k * x, k * y, k * z);
+  operator -(Vector o) => new Vector(x - o.x, y - o.y, z - o.z);
+  operator +(Vector o) => new Vector(x + o.x, y + o.y, z + o.z);
+  num dot(Vector o) => x * o.x + y * o.y + z * o.z;
+  num get length => sqrt(x * x + y * y + z * z);
+
+  Vector norm() {
+    var mag = length;
+    var div = (mag == 0) ? double.INFINITY : 1.0 / mag;
+    return this * div;
+  }
+
+  Vector cross(Vector o) =>
+      new Vector(y * o.z - z * o.y, z * o.x - x * o.z, x * o.y - y * o.x);
+}
 
 class Color {
+  final double r, g, b;
+  const Color(this.r, this.g, this.b);
+
+  Color scale(double k) => new Color(k * r, k * g, k * b);
+  Color operator +(Color v) => new Color(r + v.r, g + v.g, b + v.b);
+  Color operator *(Color v) => new Color(r * v.r, g * v.g, b * v.b);
+
   static const white = const Color(1.0, 1.0, 1.0);
   static const grey = const Color(0.5, 0.5, 0.5);
-
   static const black = const Color(0.0, 0.0, 0.0);
   static const background = Color.black;
   static const defaultColor = Color.black;
 
-  final double r, g, b;
-  const Color(this.r, this.g, this.b);
-
-  int get blue => _toInt(this.b);
-  int get green => _toInt(this.g);
-  int get red => _toInt(this.r);
-
-  Color operator *(Color v) => new Color(r * v.r, g * v.g, b * v.b);
-  Color operator +(Color v) => new Color(r + v.r, g + v.g, b + v.b);
-  Color scale(double k) => new Color(k * r, k * g, k * b);
-
-  static int _toInt(double d) => ((d.clamp(0, 1)) * 255).toInt();
+  String toDrawingColor() {
+    var legalize = (num d) => ((d > 1 ? 1 : d) * 255).toInt();
+    return "${legalize(r)}, ${legalize(g)}, ${legalize(b)}";
+  }
 }
 
 class Camera {
-  Vector3 pos, forward, right, up;
+  Vector pos, forward, right, up;
 
-  Camera(this.pos, Vector3 lookAt) {
-    Vector3 down = new Vector3(0.0, -1.0, 0.0);
-    forward = (lookAt - pos).normalize();
-    right = forward.cross(down).normalize() * 1.5;
-    up = forward.cross(right).normalize() * 1.5;
+  Camera(this.pos, Vector lookAt) {
+    Vector down = new Vector(0.0, -1.0, 0.0);
+    forward = (lookAt - pos).norm();
+    right = forward.cross(down).norm() * 1.5;
+    up = forward.cross(right).norm() * 1.5;
   }
 }
 
 class Ray {
-  final Vector3 start, dir;
+  final Vector start, dir;
   Ray(this.start, this.dir);
 }
 
@@ -50,20 +66,20 @@ class Intersection {
 }
 
 abstract class Surface {
+  Color diffuse(Vector pos);
+  Color specular(Vector pos);
+  double reflect(Vector pos);
   int get roughness;
-  Color diffuse(Vector3 pos);
-  double reflect(Vector3 pos);
-  Color specular(Vector3 pos);
 }
 
 abstract class Thing {
-  Surface get surface;
   Intersection intersect(Ray ray);
-  Vector3 normal(Vector3 pos);
+  Vector normal(Vector pos);
+  Surface get surface;
 }
 
 class Light {
-  final Vector3 pos;
+  final Vector pos;
   final Color color;
   Light(this.pos, this.color);
 }
@@ -72,35 +88,39 @@ class Scene {
   final List<Thing> things;
   final List<Light> lights;
   final Camera camera;
-
   Scene(this.things, this.lights, this.camera);
 }
 
 class Sphere implements Thing {
   final double radius2, radius;
-  final Vector3 center;
+  final Vector center;
   final Surface surface;
 
   Sphere(this.center, double radius, this.surface)
-      : this.radius = radius,
-        this.radius2 = radius * radius;
+      : radius = radius,
+        radius2 = radius * radius;
 
+  Vector normal(Vector pos) => (pos - center).norm();
   Intersection intersect(Ray ray) {
-    Vector3 eo = this.center - ray.start;
+    Vector eo = center - ray.start;
     double v = eo.dot(ray.dir);
     double dist = 0.0;
     if (v >= 0) {
-      double disc = this.radius2 - (eo.dot(eo) - v * v);
-      if (disc >= 0) dist = v - sqrt(disc);
+      double disc = radius2 - (eo.dot(eo) - v * v);
+      if (disc >= 0) {
+        dist = v - sqrt(disc);
+      }
     }
-    return dist == 0 ? null : new Intersection(this, ray, dist);
+    if (dist == 0) {
+      return null;
+    } else {
+      return new Intersection(this, ray, dist);
+    }
   }
-
-  Vector3 normal(Vector3 pos) => (pos - center).normalize();
 }
 
 class Plane implements Thing {
-  final Vector3 norm;
+  final Vector norm;
   final double offset;
   final Surface surface;
   Plane(this.norm, this.offset, this.surface);
@@ -115,29 +135,30 @@ class Plane implements Thing {
     }
   }
 
-  Vector3 normal(Vector3 pos) => norm;
+  Vector normal(Vector pos) => norm;
 }
 
-typedef Color SurfaceFunction(Vector3 pos);
-typedef num ReflectFunction(Vector3 pos);
+typedef Color SurfaceFunction(Vector pos);
+typedef num ReflectFunction(Vector pos);
 
-class CustomFunctionalSurface implements Surface {
+class CustomSurface implements Surface {
   final SurfaceFunction _diffuse, _specular;
   final ReflectFunction _reflect;
   final int roughness;
-  CustomFunctionalSurface(
-      this._diffuse, this._specular, this._reflect, this.roughness);
+  CustomSurface(this._diffuse, this._specular, this._reflect, this.roughness);
 
-  Color diffuse(Vector3 pos) => _diffuse(pos);
-  Color specular(Vector3 pos) => _specular(pos);
-  double reflect(Vector3 pos) => _reflect(pos);
+  Color diffuse(Vector pos) => _diffuse(pos);
+  Color specular(Vector pos) => _specular(pos);
+  double reflect(Vector pos) => _reflect(pos);
 }
 
+// Ugh. We're trying to emulate 'module' here. Dart programs don't use
+// classes in this way.
 class Surfaces {
-  static final Surface shiny = new CustomFunctionalSurface(
-      (_) => Color.white, (_) => Color.grey, (_) => 0.7, 250);
-  static final Surface checkerboard = new CustomFunctionalSurface(
-      (Vector3 pos) {
+  static final Surface shiny =
+      new CustomSurface((_) => Color.white, (_) => Color.grey, (_) => 0.7, 250);
+  static final Surface checkerboard = new CustomSurface(
+      (Vector pos) {
         if ((pos.z.floor() + pos.x.floor()) % 2 != 0) {
           return Color.white;
         } else {
@@ -156,7 +177,7 @@ class Surfaces {
 }
 
 class RayTracer {
-  static const int maxDepth = 5;
+  int _maxDepth = 5;
 
   Intersection _intersections(Ray ray, Scene scene) {
     double closest = double.INFINITY;
@@ -190,31 +211,31 @@ class RayTracer {
   }
 
   Color _shade(Intersection isect, Scene scene, int depth) {
-    Vector3 d = isect.ray.dir;
-    Vector3 pos = d * isect.dist + isect.ray.start;
-    Vector3 normal = isect.thing.normal(pos);
-    Vector3 reflectDir = d - normal * normal.dot(d) * 2.0;
+    Vector d = isect.ray.dir;
+    Vector pos = d * isect.dist + isect.ray.start;
+    Vector normal = isect.thing.normal(pos);
+    Vector reflectDir = d - normal * normal.dot(d) * 2.0;
     Color naturalColor = Color.background +
         _getNaturalColor(isect.thing, pos, normal, reflectDir, scene);
-    Color reflectedColor = (depth >= maxDepth)
+    Color reflectedColor = (depth >= _maxDepth)
         ? Color.grey
         : _getReflectionColor(
             isect.thing, pos, normal, reflectDir, scene, depth);
     return naturalColor + reflectedColor;
   }
 
-  Color _getReflectionColor(Thing thing, Vector3 pos, Vector3 normal,
-      Vector3 rd, Scene scene, int depth) {
+  Color _getReflectionColor(Thing thing, Vector pos, Vector normal, Vector rd,
+      Scene scene, int depth) {
     Color color = _traceRay(new Ray(pos, rd), scene, depth + 1);
     double scale = thing.surface.reflect(pos);
     return color.scale(scale);
   }
 
   Color _getNaturalColor(
-      Thing thing, Vector3 pos, Vector3 norm, Vector3 rd, Scene scene) {
+      Thing thing, Vector pos, Vector norm, Vector rd, Scene scene) {
     Color addLight(Color col, Light light) {
-      Vector3 ldis = light.pos - pos;
-      Vector3 livec = ldis.normalized();
+      Vector ldis = light.pos - pos;
+      Vector livec = ldis.norm();
       double neatIsect = _testRay(new Ray(pos, livec), scene);
       bool isInShadow =
           (neatIsect == null) ? false : (neatIsect <= ldis.length);
@@ -224,7 +245,7 @@ class RayTracer {
         double illum = livec.dot(norm);
         Color lcolor =
             (illum > 0) ? light.color.scale(illum) : Color.defaultColor;
-        double specular = livec.dot(rd.normalized());
+        double specular = livec.dot(rd.norm());
         Color scolor = (specular > 0)
             ? light.color.scale(pow(specular, thing.surface.roughness))
             : Color.defaultColor;
@@ -238,20 +259,20 @@ class RayTracer {
 
   void render(Scene scene, CanvasRenderingContext2D ctx, int screenWidth,
       int screenHeight) {
-    Vector3 getPoint(int x, int y, Camera camera) {
+    Vector getPoint(int x, int y, Camera camera) {
       var recenterX = (x) => (x - (screenWidth / 2.0)) / 2.0 / screenWidth;
       var recenterY = (y) => -(y - (screenHeight / 2.0)) / 2.0 / screenHeight;
-      Vector3 result = camera.forward +
+      Vector result = camera.forward +
           camera.right * recenterX(x) +
           camera.up * recenterY(y);
-      return result.normalize();
+      return result.norm();
     }
 
     for (var y = 0; y < screenHeight; y++) {
       for (var x = 0; x < screenWidth; x++) {
         var color = _traceRay(
             new Ray(scene.camera.pos, getPoint(x, y, scene.camera)), scene, 0);
-        ctx.setFillColorRgb(color.red, color.green, color.blue);
+        ctx.fillStyle = "rgb(${color.toDrawingColor()})";
         ctx.fillRect(x, y, x + 1, y + 1);
       }
     }
@@ -259,15 +280,15 @@ class RayTracer {
 }
 
 final Scene scene = new Scene([
-  new Plane(new Vector3(0.0, 1.0, 0.0), 0.0, Surfaces.checkerboard),
-  new Sphere(new Vector3(0.0, 1.0, -0.25), 1.0, Surfaces.shiny),
-  new Sphere(new Vector3(-1.0, 0.5, 1.5), 0.5, Surfaces.shiny)
+  new Plane(new Vector(0.0, 1.0, 0.0), 0.0, Surfaces.checkerboard),
+  new Sphere(new Vector(0.0, 1.0, -0.25), 1.0, Surfaces.shiny),
+  new Sphere(new Vector(-1.0, 0.5, 1.5), 0.5, Surfaces.shiny)
 ], [
-  new Light(new Vector3(-2.0, 2.5, 0.0), new Color(0.49, 0.07, 0.07)),
-  new Light(new Vector3(1.5, 2.5, 1.5), new Color(0.07, 0.07, 0.49)),
-  new Light(new Vector3(1.5, 2.5, -1.5), new Color(0.07, 0.49, 0.071)),
-  new Light(new Vector3(0.0, 3.5, 0.0), new Color(0.21, 0.21, 0.35))
-], new Camera(new Vector3(3.0, 2.0, 4.0), new Vector3(-1.0, 0.5, 0.0)));
+  new Light(new Vector(-2.0, 2.5, 0.0), new Color(0.49, 0.07, 0.07)),
+  new Light(new Vector(1.5, 2.5, 1.5), new Color(0.07, 0.07, 0.49)),
+  new Light(new Vector(1.5, 2.5, -1.5), new Color(0.07, 0.49, 0.071)),
+  new Light(new Vector(0.0, 3.5, 0.0), new Color(0.21, 0.21, 0.35))
+], new Camera(new Vector(3.0, 2.0, 4.0), new Vector(-1.0, 0.5, 0.0)));
 
 void main() {
   int width = 256;
@@ -293,6 +314,6 @@ void main() {
     times.add(time);
     print(times);
     i++;
-    if (i <= max) button.click();
+    if (i < max) button.click();
   });
 }
